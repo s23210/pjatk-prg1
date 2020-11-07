6886c35a33ed62f3720e5ab96733730f5eb79ba7
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-07 12:33:15 +0100

    s03-countdown.cpp

---
+++ src/s03-countdown.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+auto main(int, char* argv[]) -> int
+{
+    for(auto i = std::stoi(argv[1]); i >= 0; --i)
+    {
+        std::cout << i << "\n";
+    }
+return 0;    
+}
