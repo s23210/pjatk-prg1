56c8b0c9a7c56801036ebe14202178942949be7e
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-07 11:57:42 +0100

    s03-password.cpp

---
+++ src/s03-password.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+auto main() -> int
+{
+    auto password_in = std::string{};
+    std::cout << "set password:";
+    auto password = std::string{};
+    std::getline(std::cin, password);
+    std::cout << "type password:";
+    std::getline(std::cin, password_in);
+    while (password != password_in){
+        std::cout << "wrong password" << "\n";
+        std::cout << "type password:";
+        std::getline(std::cin, password_in);
+    }
+    std::cout << "You're logged in" << "\n";
+return 0;
+}
