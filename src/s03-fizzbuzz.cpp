3af3c68aa71411ea90f8e3c0ff73f404de94491c
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-08 20:13:05 +0100

    fizzbuz

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 auto main(int, char* argv[]) -> int
 {
-   auto n = stoi(argv[1]);
-    for (auto i = 1; i <= n; ++i) {    
+    auto n = stoi(argv[1]);
+    for (auto i = 1; i <= n; ++i) {
         auto a = i % 3;
         auto b = i % 5;
         cout << endl << i;
-        if (a == 0 && b == 0) {cout << "FizzBuzz";}
-            else {
-            if (a == 0) {cout << "Fizz";}
-            if (b == 0) {cout << "Buzz";}
+        if (a == 0 && b == 0) {
+            cout << "FizzBuzz";
+        } else {
+            if (a == 0) {
+                cout << "Fizz";
             }
+            if (b == 0) {
+                cout << "Buzz";
+            }
+        }
     }
-cout << endl;
-return 0;
+    cout << endl;
+    return 0;
 }
