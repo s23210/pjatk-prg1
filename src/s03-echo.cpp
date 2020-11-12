e2d9765381e70a839848906f5cb772de5ef17687
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-12 23:55:15 +0100

    s03-echo.cpp

---
+++ src/s03-echo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int
+{
+    // nie drukuje nowej linii na końcu programu
+    if (std::string(argv[1]) == "-n") {
+        for (int i = 2; i <= argc; ++i) {
+            std::cout << argv[i] << " ";
+        }
+    }
+    // drukuje argumenty w odwrotnej kolejności
+    if (std::string(argv[1]) == "-r") {
+        for (int i = argc - 1; i >= 2; --i) {
+            std::cout << argv[i] << " ";
+        };
+        std::cout << "\n";
+    }
+    // drukuje argumenty po jednym na linię
+    if (std::string(argv[1]) == "-l") {
+        for (int i = 2; i <= argc; ++i) {
+            std::cout << argv[i] << "\n";
+        };
+    }
+    // basic print
+    if (std::string(argv[1]) != "-n" && std::string(argv[1]) != "-r"
+        && std::string(argv[1]) != "-l") {
+        for (int i = 1; i <= argc; ++i) {
+            std::cout << argv[i] << " ";
+        };
+        std::cout << "\n";
+    }
+    return 0;
+}
