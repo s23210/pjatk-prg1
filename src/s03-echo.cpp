16e6b6ddd7e714debc0096d23fa08a946ab42e4a
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-13 00:53:52 +0100

    s03-echo.cpp  final version

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -35,12 +35,12 @@ auto main(int argc, char* argv[]) -> int
         }
     }
     // basic print
-     if (std::string(argv[1]) != "-n" && std::string(argv[1]) != "-r"
+    if (std::string(argv[1]) != "-n" && std::string(argv[1]) != "-r"
         && std::string(argv[1]) != "-l") {
         for (int i = 1; i <= argc; ++i) {
             std::cout << argv[i] << " ";
         }
-        printf ("\n");
+        printf("\n");
     }
     return 0;
 }
