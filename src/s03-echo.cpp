dac74c8a2f600c0b77fe93adc224eac6ecb3870c
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-13 00:51:00 +0100

    s03-echo.cpp final version

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -11,24 +11,36 @@ auto main(int argc, char* argv[]) -> int
     }
     // drukuje argumenty w odwrotnej kolejności
     if (std::string(argv[1]) == "-r") {
-        for (int i = argc - 1; i >= 2; --i) {
-            std::cout << argv[i] << " ";
-        };
-        std::cout << "\n";
+        if (std::string(argv[2]) != "-n" && std::string(argv[2]) != "-l") {
+            for (int i = argc - 1; i >= 2; --i) {
+                std::cout << argv[i] << " ";
+            }
+            std::cout << "\n";
+        }
+        if (std::string(argv[2]) == "-n") {
+            for (int i = argc - 1; i >= 3; --i) {
+                std::cout << argv[i] << " ";
+            }
+        }
+        if (std::string(argv[2]) == "-l") {
+            for (int i = argc - 1; i >= 3; --i) {
+                std::cout << argv[i] << "\n";
+            }
+        }
     }
     // drukuje argumenty po jednym na linię
     if (std::string(argv[1]) == "-l") {
         for (int i = 2; i <= argc; ++i) {
             std::cout << argv[i] << "\n";
-        };
+        }
     }
     // basic print
-    if (std::string(argv[1]) != "-n" && std::string(argv[1]) != "-r"
+     if (std::string(argv[1]) != "-n" && std::string(argv[1]) != "-r"
         && std::string(argv[1]) != "-l") {
         for (int i = 1; i <= argc; ++i) {
             std::cout << argv[i] << " ";
-        };
-        std::cout << "\n";
+        }
+        printf ("\n");
     }
     return 0;
 }
