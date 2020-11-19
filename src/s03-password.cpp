07085486e9a9584a934fc108adf211f0d1e048f8
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-20 00:11:42 +0100

    s03-password bugfix

--- src/s03-password.cpp
+++ src/s03-password.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 #include <string>
 
-auto main() -> int
+auto main(int, char* argv[]) -> int
 {
+    auto password = std::string{argv[1]};
     auto password_in = std::string{};
-    std::cout << "set password:";
-    auto password = std::string{};
-    std::getline(std::cin, password);
     std::cout << "type password:";
     std::getline(std::cin, password_in);
     while (password != password_in) {
-        std::cout << "wrong password"
-                  << "\n";
         std::cout << "type password:";
         std::getline(std::cin, password_in);
     }
-    std::cout << "You're logged in"
-              << "\n";
     return 0;
 }
