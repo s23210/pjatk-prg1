2794298032e97df80c8375dbc1e679de17ab46f8
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-20 18:19:24 +0100

    s03-password bug fix and optimization

--- src/s03-password.cpp
+++ src/s03-password.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
 
+auto ask_user_for_password() -> std::string
+{
+    std::cout << "type password:";
+    auto n = std::string{};
+    std::getline(std::cin, n);
+    return std::string(n);
+}
+
 auto main(int, char* argv[]) -> int
 {
-    auto password = std::string{argv[1]};
-    auto password_in = std::string{};
-    std::cout << "type password:";
-    std::getline(std::cin, password_in);
+    auto password = std::string{argv[1]};    
+    auto password_in = ask_user_for_password();
     while (password != password_in) {
-        std::cout << "type password:";
-        std::getline(std::cin, password_in);
+        auto password_in = ask_user_for_password();
+        if(password == password_in){break;}
     }
     return 0;
 }
