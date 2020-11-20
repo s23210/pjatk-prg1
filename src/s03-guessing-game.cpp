4933248445ad4f2ce35e90e7cb89c81d94e26100
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-20 21:16:39 +0100

    s03-guessing-game bug fixes and optimization

--- src/s03-guessing-game.cpp
+++ src/s03-guessing-game.cpp
@@ -15,9 +15,10 @@ auto main() -> int
     std::random_device rd;
     std::uniform_int_distribution<int> d100(1, 100);
 
-    int n = d100(rd);
-    do {
-        auto in = ask_user_for_integer();
+    int n   = d100(rd);
+    auto in = ask_user_for_integer();
+
+    while (in != n) {
         if (in > n) {
             std::cout << "number is too big!"
                       << "\n";
@@ -26,10 +27,11 @@ auto main() -> int
             std::cout << "number is too low!"
                       << "\n";
         }
-        if (in == n) {
-            std::cout << "you're correct"
-                      << "\n";
-        }
-    } while (in != n);
+        in = ask_user_for_integer();
+    }
+    if (in == n) {
+        std::cout << "you're correct"
+                  << "\n";
+    }
     return 0;
 }
