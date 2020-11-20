2f00c6c24f199d944e616c697d570b308133c344
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-20 22:00:14 +0100

    s03-fizzbuzz bugfixes and optimization

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
-auto main(int, char* argv[]) -> int
+auto main(int argc, char* argv[]) -> int
 {
+    if (argc == 1) {
+        std::cerr << "no argument given on cmd"
+                  << "\n";
+    }
+
     auto n = std::stoi(argv[1]);
+
     for (auto i = 1; i <= n; ++i) {
         auto a = i % 3;
         auto b = i % 5;
-        std::cout << i;
-        if (a == 0 && b == 0) {
-            std::cout << " FizzBuzz";
-        } else {
-            if (a == 0) {
-                std::cout << " Fizz";
-            }
-            if (b == 0) {
-                std::cout << " Buzz";
-            }
+        std::cout << i << " ";
+        if (a == 0) {
+            std::cout << "Fizz";
+        }
+        if (b == 0) {
+            std::cout << "Buzz";
         }
         std::cout << "\n";
     }
