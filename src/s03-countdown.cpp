8a347d2fbf6fdb19038907c77afcaead21de0c95
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2020-11-20 22:06:15 +0100

    s03-countdown bug fix

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 
-auto main(int, char* argv[]) -> int
+auto main(int argc, char* argv[]) -> int
 {
+    if (argc == 1) {
+        std::cerr << "no argument given on cmd"
+                  << "\n";
+    }
+
     for (auto i = std::stoi(argv[1]); i >= 0; --i) {
         std::cout << i << "\n";
     }
